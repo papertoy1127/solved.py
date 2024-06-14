1aa46c42e236042f9531c8567540947d11bebd41
papertoy1127 <richard1909@naver.com>
2024-06-15 02:26:26 +0900

    0.1.10

--- solvedac/schemes/Grass.py
+++ solvedac/schemes/Grass.py
@@ -7,8 +7,8 @@ from datetime import datetime
 
 class Grass(Repr):
     def __init__(self, data: JObject):
-        self.grass: Sequence[History[int|Literal['frozen']]] = list(map(lambda x: History(datetime.strptime(x["date"], "%Y-%m-%d").date(), x["value"]), cast(Sequence[Any], data["grass"])))
-        self.theme = GrassTheme(cast(str, data["theme"]))
+        self.grass: Sequence[History[int|Literal['frozen','repaired-incremented']]] = list(map(lambda x: History(datetime.strptime(x["date"], "%Y-%m-%d").date(), x["value"]), cast(Sequence[Any], data["grass"])))
+        self.theme = cast(str, data["theme"])
         self.currentStreak = cast(int, data["currentStreak"])
         self.longestStreak = cast(int, data["longestStreak"])
         self.topic = GrassTopic(cast(int, data["topic"]))
